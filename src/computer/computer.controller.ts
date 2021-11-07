87864aa572b4e2c440d66a7e51cf882a6bc19976
Khalled MENEOUALI <khalled.meneouali@external.totalenergies.com>
2021-11-07 14:30:16 +0100

    injection of dependance for multiple Modules

---
+++ src/computer/computer.controller.ts
@@ -0,0 +1,19 @@
+import { DiskService } from './../disk/disk.service';
+import { CpuService } from './../cpu/cpu.service';
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('computer')
+export class ComputerController {
+
+    constructor(
+        private cpuService:CpuService,
+        private diskService:DiskService
+    ){
+
+    }
+
+    @Get()
+    run(){
+        return [this.cpuService.computer(1,2),this.diskService.getData()]
+    }
+}
---
+++ src/cpu/cpu.service.ts
@@ -0,0 +1,16 @@
+import { PowerService } from './../power/power.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CpuService {
+    constructor(private powerService:PowerService){
+
+    }
+
+    computer(a:number,b:number){
+        console.log('Drawing 10 watts of power from Power Service');
+        this.powerService.supplyPower(10);
+        return a+b;
+
+    }
+}
